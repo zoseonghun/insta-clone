ac21dc74e2203aa1f13934003c5a2292a32f7e1f
Java <zoseonghun@gmail.com>
2025-03-15 20:08:39 +0900

    #03 [FE/feat] close create feed modal

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -1,21 +1,51 @@
 
 
 // 피드 생성 모달을 전역관리
-let $modal = null;
-// 모달 관련 JS 함수 - 외부에 노출
-function initCreateFeedModal() {
-    $modal = document.getElementById('createPostModal');
+let $modal = document.getElementById('createPostModal');
 
+// 모달 관련 DOM들을 저장할 객체
+let elements = {
+    $closeBtn : $modal.querySelector('.modal-close-button'),
+    $backdrop : $modal.querySelector('.modal-backdrop'),
+};
+
+// 피드 생성 모달 관련 이벤트 함수
+function setUpModalEvents() {
+
+    const {$closeBtn, $backdrop} = elements;
     // 모달 열기 함수
     const openModal = e => {
         e.preventDefault();
         // 모달 열기
         $modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden'; // 배경 바디 스크롤 방지
     };
+
+    // 모달 닫기
+    const closeModal = e => {
+        e.preventDefault();
+        $modal.style.display = 'none';
+        document.body.style.overflow = 'flex'; // 배경 바디 스크롤 방지 해제
+    };
+
     // 피드 생성 모달 열기 이벤트
     document.querySelector('.fa-square-plus')
         .closest('.menu-item')
         .addEventListener('click', openModal);
+
+    // X버튼 눌렀을 때
+    $closeBtn.addEventListener('click', closeModal);
+    // 백드롭 눌렀을 때
+    $backdrop.addEventListener('click', closeModal)
+}
+// 이벤트 바인딩 관련 함수
+function bindEvents() {
+    setUpModalEvents();
+}
+
+// 모달 관련 JS 함수 - 외부에 노출
+function initCreateFeedModal() {
+    bindEvents();
 }
 
 export default initCreateFeedModal;
