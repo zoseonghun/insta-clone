d94b54bd63b2b421aefe6018bb8f97f71ab34d0b
Java <zoseonghun@gmail.com>
2025-03-17 22:34:35 +0900

    encoding password

--- src/main/java/com/example/instagramclone/service/MemberService.java
+++ src/main/java/com/example/instagramclone/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.member.dto.request.SignUpRequest;
+import com.example.instagramclone.domain.member.entity.Member;
 import com.example.instagramclone.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +15,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
 
     // 회원가입 중간처리
     public void signUp(SignUpRequest signUpRequest) {
 
-        // 회원정보를 데이터베이스에 저장
-        memberRepository.insert(signUpRequest.toEntity());
+        // 순수 비밀번호를 꺼내서 암호화
+        String rawPasswrod = signUpRequest.getPassword();
+        // 암호화된 패스워드
+        String encodedPassword = passwordEncoder.encode(rawPasswrod);
+
+        // 회원정보를 엔터티로 변환
+        Member newMember = signUpRequest.toEntity();
+        // 패스워드를 인코딩패스워드로 교체
+        newMember.setPassword(encodedPassword);
+
+        // DB에 전송
+        memberRepository.insert(newMember);
     }
 
 }
