792b952defdfe1c3b86c04a64294f2c4fc73d01c
Java <zoseonghun@gmail.com>
2025-03-17 18:04:31 +0900

    add spring security dependency and make SecurityConfig

---
+++ src/main/java/com/example/instagramclone/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.instagramclone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 커스텀 시큐리티 설정파일이라는 의미
+public class SecurityConfig {
+
+    // 시큐리티 필터체인 빈을 등록
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 커스텀 보안 설정
+        http
+                .csrf(CsrfConfigurer<HttpSecurity>csrf -> csrf.disable())
+                .cors(CorsConfigurer<HttpSecurity>cors -> cors.configure(http))
+
+                ;
+
+        return http.build();
+    }
+}
