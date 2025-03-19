a4ad0822659fbc377c4eae02ad76e05aad50af09
Java <zoseonghun@gmail.com>
2025-03-20 01:40:16 +0900

    jwt basic setting (build.gradle, application.yml)

--- src/test/java/com/example/instagramclone/instagramclone/InstagramCloneTemplateApplicationTests.java
+++ src/test/java/com/example/instagramclone/instagramclone/InstagramCloneTemplateApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.instagramclone.instagramclone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @SpringBootTest
 class InstagramCloneTemplateApplicationTests {
 
     @Test
     void contextLoads() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] keyBytes = new byte[32];
+        secureRandom.nextBytes(keyBytes);
+        String secretKey = Base64.getEncoder().encodeToString(keyBytes);
+        System.out.println("Generated Secret Key: " + secretKey);
     }
 
 }
